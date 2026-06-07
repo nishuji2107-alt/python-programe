59959f8e6c6fb9670883bfd735e87a8666e01618
Nishant Rathore <nishuji2107@gmail.com>
2026-06-07 21:58:03 +0530

    Add files via upload

---
+++ prog.to calculate area and peri. of pall..py
@@ -0,0 +1,8 @@
+l=float(input("Enter length: "))
+w=float(input("Enter width: "))
+h=float(input("Enter height: "))
+area_parallelogram=l*h
+peri_parallelogram=2*1 + 2*w
+print( "The area of the parallelogram is ", area_parallelogram)
+print( "The perimeter of the parallelogram is ", peri_parallelogram)
+
