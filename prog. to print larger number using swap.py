7c05c942a91624662c685ce70e2550906fc7082e
Nishant Rathore <nishuji2107@gmail.com>
2026-06-10 22:25:06 +0530

    Add files via upload

---
+++ prog. to print larger number using swap.py
@@ -0,0 +1,11 @@
+x=y=0
+x=int(input("Enter the first number: "))
+y=int(input("Enter the second number: "))
+if(x>y):
+    print("The larger number is : ",y)
+    print("The smaller number is : ",x)
+else:
+    print("The larger number is : ",y)
+    print("The smaller number is : ",x)
+     
+    
