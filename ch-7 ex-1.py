70d4516959dddcd7617cad9fc69b7121b73210c3
Nishant Rathore <nishuji2107@gmail.com>
2026-06-11 21:52:29 +0530

    Add files via upload

---
+++ ch-7 ex-1.py
@@ -0,0 +1,7 @@
+list_of_elements=[]
+print("Enter list of five integers")
+for i in range(5):
+    print("Enter integer", i)
+    userinput=int(input())
+    list_of_elements.append(userinput)
+print("The list is: ",list_of_elements)
